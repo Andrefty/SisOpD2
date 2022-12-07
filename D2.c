5037cf784c23efb48efaba3f23d55d0fd91a2bfa
a <39839328+Andrefty@users.noreply.github.com>
2022-12-07 13:07:49 +0200

    Dupa "corectare" Dragulici

--- D2.c
+++ D2.c
@@ -37,6 +37,12 @@ int calcul(int argc, char *argv[])
     while ((direntry = readdir(dir)) != NULL)
     {
         char *filepath = malloc(strlen(dirpath) + strlen(direntry->d_name) + 2);
+        if (filepath == NULL)
+        {
+            perror("malloc");
+            closedir(dir); 
+            exit(1);
+        }
         strcpy(filepath, dirpath);
         strcat(filepath, "/");
         strcat(filepath, direntry->d_name);
@@ -44,6 +50,8 @@ int calcul(int argc, char *argv[])
         if (stat(filepath, &buf) < 0)
         {
             perror("stat");
+            free(filepath);
+            closedir(dir);
             exit(1);
         }
         if (S_ISDIR(buf.st_mode) && strcmp(direntry->d_name, ".") == 0)
@@ -56,7 +64,7 @@ int calcul(int argc, char *argv[])
         }
         else if (S_ISDIR(buf.st_mode) && strcmp(direntry->d_name, ".") != 0 && strcmp(direntry->d_name, "..") != 0)
         {
-            size += calcul(2, (char *[]){"", filepath});
+            size += calcul(2, (char *[]){"", filepath, NULL});
         }
     }
 
@@ -71,31 +79,16 @@ int calcul(int argc, char *argv[])
 int main(int argc, char *argv[])
 {
     long int folderSize = 0;
+    if (argc != 2)
+    {
+        fprintf(stderr,"Usage: %s <cale_director>", argv[0]);
+        exit(1);
+    }
+    if (strcmp(argv[1],"")==0)
+    {
+        fprintf(stderr,"Sir %s vid\n", argv[1]);
+        exit(1);
+    }
     folderSize = calcul(argc, argv);
     printf("Marime folder: %ld b (%lf kb / %lf Mb)\n", folderSize, folderSize / 1024.0, folderSize / (1024 * 1024.0));
 }
-
-// // Path: D2dela1.c
-// // Program care primeste ca argument in linia de comanda un
-// //  director si calculeaza suma dimensiunilor fisierelor din arborescenta cu
-// //  originea in el.
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <sys/types.h>
-// #include <sys/stat.h>
-// #include <unistd.h>
-// #include <dirent.h>
-// #include <string.h>
-
-// int main(int argc, char *argv[])
-// {
-//     DIR *dir;
-//     struct dirent *direntry;
-//     struct stat buf;
-//     char *dirpath;
-//     int size = 0;
-
-//     if (argc != 2)
-//     {
-//         printf("Usage: %
